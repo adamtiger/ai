49e6880a17cb62a1e55e25ed0d756c9793021f24
Adam Budai <budai8adam@gmail.com>
2016-11-01 19:18:19 +0100

    The Solution WindyGridWorld was created. The UI was created as well.

---
+++ WindyGridWorld/WindyGridWorld.GUI/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindyGridWorld.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void rows_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+        // Starting the reinforcement learning process.
+        // Read the initializing data.
+        private void startRL_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Load the given episode.
+        private void getEpsiode_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Handle the agent.
+        // Set back the agent to the starting position.
+        private void zeros_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Step to the next position on the grid.
+        private void next_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Show the learning curve.
+        private void learningCv_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
