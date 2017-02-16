48c5c3b5ae0b64a9479dcfe7b0a916ee9e4c5f4e
Adam Budai <budai8adam@gmail.com>
2017-02-16 16:37:58 +0100

    The image reader modul was finished.

--- CV4sensorhubRL/cv4reader/cv4reader/pool.py
+++ CV4sensorhubRL/cv4reader/cv4reader/pool.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import misc
+import os
 
 class Image:
 
@@ -8,22 +9,42 @@ class Image:
         self.sgm_img = segmented_img
         
     def get_pixel_base(self, x, y):
-        pass
+        return self.base_img[x, y]
     
     def get_pixel_sgm(self, x, y):
-        pass
+        return self.sgm_img[x, y]
         
 
 class ImagePool:
 
-    def __init__(self, size_pool):
+    def __init__(self, size_pool, refresh_freq):
+        self.refresh_freq = refresh_freq
         self.size = size_pool
         self.images = [0]*size_pool
         self.idx = 0
-        
-    def add(self, image):
-        self.images[self.idx] = image
-        self.idx = (self.idx + 1) % size_pool
+        self.cntr = 0
+        self.file_list = os.listdir("images")
+
+        assert len(self.file_list) % 2 == 0, "A solution or a base image is missing!"
         
     def get_random_img(self):
-        self
+        if self.cntr == refresh_freq:
+            __refresh()
+            self.cntr = 0
+        self.cntr += 1
+        
+        rand_idx = np.random.randint(0, self.size-1)
+        return self.images[rand_idx]
+
+    def __refresh(self):
+        rand_idxs = np.random.randint(0, self.file_list.size/2-1, (self.size))
+
+        for idx in rand_idxs:
+            base_img = scipy.misc.imread("images/" + self.file_list[idx])
+            sgm_img = scipy.misc.imread("images/" + self.file_list[idx + 1])
+            img = Image(base_img, sgm_img)
+            __add(img)
+        
+    def __add(self, image):
+        self.images[self.idx] = image
+        self.idx = (self.idx + 1) % self.size    
--- CV4sensorhubRL/cv4reader/cv4reader/reader.py
+++ CV4sensorhubRL/cv4reader/cv4reader/reader.py
@@ -1,9 +1,9 @@
-
+import pool
 
 class Cv4Reader:
 
-    def __init__(self, size_of_pool):
-        pass
+    def __init__(self, size_of_pool, refresh_freq):
+        self.image_pool = pool.ImagePool(size_of_pool, refresh_freq)
         
     def next_image(self):
-        pass    
+        return self.image_pool.get_random_img()    
