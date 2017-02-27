3f5e44e597b225436b068190c83789f8378d4622
Adam Budai <budai8adam@gmail.com>
2017-02-27 18:47:02 +0100

    Tests were created for PreProcessing.

--- A3C/test.py
+++ A3C/test.py
@@ -1,2 +1,48 @@
-# -*- coding: utf-8 -*-
+from preproc import PreProcessing as  prep 
+import gym
+
+# Creating variables to use it anywhere
+
+env = gym.make("Breakout-v0")
+
+
+
+# Test the preprocessing functions:
+
+def _init_preproc_test():
+    return prep(env)
+
+def test_preprocessing():
+    
+    p = _init_preproc_test()
+    o,r,d,i = env.step(1)
+    state = p.preprocessing(o)
+
+    print "test_preprocessing: successful"
+
+
+def test_preproc():
+    test_preprocessing()
+
+
+# Test the A3C class:
+
+
+# Test the NN class:
+
+
+
+
+# ------------------------------------
+
+def run_all():
+    
+    test_preproc()
+
+    print "All tests were SUCCESSFUL."
+
+
+# RUN TESTS
+
+run_all()
 
