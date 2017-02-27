5c15934e0d81759498054c2a489687007384a9f1
Adam Budai <budai8adam@gmail.com>
2017-02-27 18:27:34 +0100

    The A3C algorithm was implemented without the neural network functionalities.

--- A3C/a3c.py
+++ A3C/a3c.py
@@ -6,7 +6,7 @@ Created on Tue Feb 21 22:00:02 2017
 """
 import threading
 
-import preproc
+from preproc import PreProcessing 
 import nn
 import logger
 
@@ -20,22 +20,45 @@ class A3C(threading.Thread):
         self.environment = gym.make(atari_env_name)
         self.params = params
         self.T = IterationNum(self.params.max_it(), threading.Lock())
+        self.preprocessor = PreProcessing(self.environment)
         self.t = 1
         self.neural = nn.create_nn()
         
         
     def run(self):
-        gradients = self.neural.create_gradientVec()
+        gradients = self.neural.create_gradient_vec()
         t_start = 0
+        state = self.preprocessor(self.environment.step(0)) 
         while self.T.should_continue:
             self.neural.to_zero(gradients)
             self.neural.synchronize(self.params.shared())
             t_start = self.t
-            
+            store_st_rw = [()]*self.params.t_max()
+            idx = 0
+            while not done and self.t - t_start < self.params.t_max():
+               state,reward, done =  self._choose_action(state)
+               store_st_rw[idx] = (state, reward)
+               self.t += 1
+               self.T.increment()
+               idx += 1
+            R = 0
+            if not done:
+                R = self.neural.V(state)
+            idx = self.t - 1
+            while self.t >= t_start:
+                R = store_st_rw[idx-t_start][1] + self.params.gamma() * R
+                self.neural.accumulate_gradients(R,store_st_rw[idx-t_start][0])
+                idx -= 1
+            self.neural.async_update(gradients)
+
+    def _choose_action(self, state):
+        pass
+
 
 class IterationNum:
     
     def __init__(self, max_it, lock):
+        
         self.max_it
         self.it = 0
         self.lock = lock
@@ -46,19 +69,26 @@ class IterationNum:
     def increment(self):
         self.lock.acquire()
         self.it += 1
+        
         self.lock.release()
     
 class Parameters:
     
-    def __init__(self, max_it, shared):
+    def __init__(self, max_it, shared, t_max, gamma):
         self.max_it = max_it
         self.shared = shared
-        
+        self.t_max = t_max
+        self.gamma = gamma
         
     def max_it(self):
         return self.max_it
         
     def shared(self):
         return self.shared
-    
+
+    def t_max(self):
+        return self.t_max
+
+    def gamma(self):
+        return self.gamma
     
--- A3C/nn.py
+++ A3C/nn.py
@@ -1,2 +1,34 @@
-# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import numpy as np
+
+
+class NN:
+
+    def __init__(self, idn):
+        self.idn  = idn
+
+    def create_nn(self):
+        pass
+
+    def create_gradient_vec(self):
+        pass
+
+    def to_zero(self, gradients):
+        pass
+
+    def synchronize(self, shared_params):
+        pass
+
+    def V(self, state):
+        pass
+
+    def accumulate_gradients(self, R, state):
+        pass
+    
+    def async_update(self, gradients):
+        pass
+
+
+
 
