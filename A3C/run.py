af1dfe6dcaeb2bec9c116e5ff8126ce1d8139aa8
Adam Budai <budai8adam@gmail.com>
2017-03-02 04:27:35 +0100

    The threading solution was changed to process handling in A3C. GIL makes difficult to efficiently use all of the cores.

--- A3C/run.py
+++ A3C/run.py
@@ -1,31 +1,47 @@
 # -*- coding: utf-8 -*-
 
-from  a3c import A3C, Parameters
+import a3c
 import nn
 
-max_it = 10000
+import argparse 
+import torch
+import torch.multiprocessing as mp
+
+
+# Parameter settings
+parser = argparse.ArgumentParser(descritpion='A3C algorithm')
+
+parser.add_argument('--num-actions', type=int, default=6, metavar='N',
+        help='the number of possible actions available (default:6, Breakout)')
+parser.add_argument('--lr', type=float, default=0.00025, metavar='F',
+        help='the learning rate (default:0.00025)')
+parser.add_argument('--num-agents', type=int, default=4, metavar='N',
+        help='the number of the agents (default:4)')
+parser.add_argument('--max-it', type=int, default=10000, metavar='N',
+        help='the number of individual steps (default:10000)')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='F',
+        help='the discounting factor (default:0.99)')
+parser.add_argument('--atari-env', default='Breakout-v0', metavar='S',
+        help='the Atari environment (default:Breakout-v0)')
+
+# Instantiation:
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    torch.manual_seed(1)
+
+    shared_nn = a3c.shared_nn()
+    shared_nn.shared_memory()
+
+    processes = []
+    for agent in range(args.num_agents):
+        a = mp.Process(target=a3c.a3c, args=(args, shared_nn))
+        a.start()
+        processes.append(a)
+    for p in processes:
+        p.join()
 
 
 
-class Launch:
-    
-    def __init__(self, num_threads, atari_name):
-        self.shared_vectors = nn.CreateShared()
-        self.parameters = Parameters(max_it, self.shared_vectors)
-        self.threads = []
 
-        for idx in range(0, num_threads):
-            self.threads.append(A3C(atari_name, self.parameters))
-            
-
-    def start(self):
-        for idx in range(0, len(self.threads)):
-            self.threads[idx].start()
-        
-
-
-# RUN the algorithm
-
-launching = Launch(2, "Breakout-v0")
-
-launching.start()
