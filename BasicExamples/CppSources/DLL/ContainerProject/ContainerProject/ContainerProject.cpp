c5af8d423369782c1c209e10e9314de98073fce3
Adam Budai <budai8adam@gmail.com>
2017-03-12 22:59:50 +0100

    A jupyter notebook was created for demonstrating the learning algorithms.

--- BasicExamples/CppSources/DLL/ContainerProject/ContainerProject/ContainerProject.cpp
+++ BasicExamples/CppSources/DLL/ContainerProject/ContainerProject/ContainerProject.cpp
@@ -1,6 +1,48 @@
-// ContainerProject.cpp : Defines the exported functions for the DLL application.
-//
-
 #include "stdafx.h"
+
+#include "ContainerProject.h"
+
+// NArmedBandit functions:
+
+#include "Stationary.h"
+#include "NonStationary.h"
+
+float* StationaryAnalysis()
+{
+	bandit::stationary::Stationary stn;
+
+	for (int i = 0; i < 10000; ++i)
+		stn.DoTrial();
+
+	float* values = new float[10];
+	for (int i = 0; i < 10; ++i) {
+
+		values[i] = stn.GetValue(i);
+	}
+
+	return values;
+}
+
+float* NonStationaryAnalysis()
+{
+	bandit::nonstationary::NonStationary nstn;
+
+	for (int i = 0; i < 10000; ++i) {
+
+		nstn.DoTrial();
+
+		if (i % 500 == 0)
+			nstn.ChangeEnv();
+	}
+
+
+	float* values = new float[10];
+	for (int i = 0; i < 10; ++i) {
+
+		values[i] = nstn.GetValue(i);
+	}
+
+	return values;
+}
 
 
--- BasicExamples/CppSources/DLL/ContainerProject/ContainerProject/ContainerProject.h
+++ BasicExamples/CppSources/DLL/ContainerProject/ContainerProject/ContainerProject.h
@@ -1,6 +1,16 @@
 #ifndef __CONTAINER_PROJECT__
 #define __CONTAINER_PROJECT__
 
+// NArmedBandit functions
+
+extern "C" __declspec(dllexport) float* StationaryAnalysis();
+
+extern "C" __declspec(dllexport) float* NonStationaryAnalysis();
+
+// MountainCar functions
+
+
+// Qlearning functions
 
 
 
