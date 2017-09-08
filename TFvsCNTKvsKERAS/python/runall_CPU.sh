d239349615c8516b0503a58452078ec1a8e917c7
Adam <budai8adam@gmail.com>
2017-09-08 10:32:31 +0200

    Errors were fixed in running keras.

--- TFvsCNTKvsKERAS/python/runall_CPU.sh
+++ TFvsCNTKvsKERAS/python/runall_CPU.sh
@@ -9,6 +9,18 @@ rl=500
 rnn=500
 conv=500
 
+echo "KERAS tests with TF backend."
+CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=tensorflow python keras_VGG.py --num-runs $vgg --file-name $file_name
+CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=tensorflow python keras_RL.py --num-runs $rl --file-name $file_name
+CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=tensorflow python keras_RNN.py --num-runs $rnn --file-name $file_name
+CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=tensorflow python keras_CONV.py --num-runs $conv --file-name $file_name
+
+echo "KERAS tests with CNTK backend."
+CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=cntk python keras_VGG.py --num-runs $vgg --file-name $file_name
+CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=cntk python keras_RL.py --num-runs $rl --file-name $file_name
+CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=cntk python keras_RNN.py --num-runs $rnn --file-name $file_name
+CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=cntk python keras_CONV.py --num-runs $conv --file-name $file_name
+
 echo "CNTK tests."
 CUDA_VISIBLE_DEVICES="" python cntk_VGG.py --num-runs $vgg --file-name $file_name 
 CUDA_VISIBLE_DEVICES="" python cntk_RL.py --num-runs $rl --file-name $file_name
@@ -20,15 +32,3 @@ CUDA_VISIBLE_DEVICES="" python tf_VGG.py --num-runs $vgg --file-name $file_name
 CUDA_VISIBLE_DEVICES="" python tf_RL.py --num-runs $rl --file-name $file_name
 CUDA_VISIBLE_DEVICES="" python tf_RNN.py --num-runs $rnn --file-name $file_name
 CUDA_VISIBLE_DEVICES="" python tf_CONV.py --num-runs $conv --file-name $file_name
-
-echo "KERAS tests with TF backend."
-CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=tensorflow python -c "from keras import backend" python keras_VGG.py --num-runs $vgg --file-name $file_name
-CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=tensorflow python -c "from keras import backend" python keras_RL.py --num-runs $rl --file-name $file_name
-CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=tensorflow python -c "from keras import backend" python keras_RNN.py --num-runs $rnn --file-name $file_name
-CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=tensorflow python -c "from keras import backend" python keras_CONV.py --num-runs $conv --file-name $file_name
-
-echo "KERAS tests with CNTK backend."
-CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=cntk python -c "from keras import backend" python keras_VGG.py --num-runs $vgg --file-name $file_name
-CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=cntk python -c "from keras import backend" python keras_RL.py --num-runs $rl --file-name $file_name
-CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=cntk python -c "from keras import backend" python keras_RNN.py --num-runs $rnn --file-name $file_name
-CUDA_VISIBLE_DEVICES="" KERAS_BACKEND=cntk python -c "from keras import backend" python keras_CONV.py --num-runs $conv --file-name $file_name
